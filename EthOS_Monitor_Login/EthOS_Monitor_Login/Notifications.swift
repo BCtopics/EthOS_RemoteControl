4edfd06d7b339df4407e4b2dd79d57ad2a0d4f46
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-09-15 12:47:09 -0400

    Add Notifications Helper

---
+++ EthOS_Monitor_Login/EthOS_Monitor_Login/Notifications.swift
@@ -0,0 +1,13 @@
+//
+//  Notifications.swift
+//  EthOS_Monitor_Login
+//
+//  Created by Bradley GIlmore on 9/15/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let refreshTableView = Notification.Name("refreshTableView")
+}
