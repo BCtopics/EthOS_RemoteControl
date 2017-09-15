4f57fde740d25c288ad56d8ab07429f60c9a58d0
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-09-15 13:03:08 -0400

    Create Account Custom Cell

---
+++ EthOS_Monitor_Login/EthOS_Monitor_Login/AccountsTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  AccountsTableViewCell.swift
+//  EthOS_Monitor_Login
+//
+//  Created by Bradley GIlmore on 9/15/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import UIKit
+
+class AccountsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var accountNameLabel: UILabel!
+    
+}
