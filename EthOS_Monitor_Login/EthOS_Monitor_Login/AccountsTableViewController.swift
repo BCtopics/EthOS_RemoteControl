e2f525b300abb5d98a2e8848d7979f42053674b8
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2019-10-22 17:24:04 -0400

    Code Cleanup, Finish cleanup of AccountsTableViewController

--- EthOS_Monitor_Login/EthOS_Monitor_Login/AccountsTableViewController.swift
+++ EthOS_Monitor_Login/EthOS_Monitor_Login/AccountsTableViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 import CoreData
 
 class AccountsTableViewController: UIViewController, NSFetchedResultsControllerDelegate, UITableViewDataSource, UITableViewDelegate {
+    
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var noAccountsView: UIView!
 
     //MARK: - LifeCycle
     
@@ -49,22 +54,23 @@ class AccountsTableViewController: UIViewController, NSFetchedResultsControllerD
         return cell
     }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            let account = AccountController.shared.fetchResultsController.object(at: indexPath)
+            AccountController.shared.remove(account: account)
+        }
+    }
+    
     //MARK: - Refreshing
     
     @objc func reloadTableData() {
         self.tableView.reloadData()
         self.areAccountsThere()
     }
-    
-    //MARK: - IBOutlets
-    
-    @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var noAccountsView: UIView!
-    
+
     //MARK: - IBActions
-    
-    // Add Button
-    
+
     @IBAction func AddButtonTapped(_ sender: Any) {
         
         let alert = UIAlertController(title: "EthOS Account", message: "Please enter you're EthOS Information", preferredStyle: .alert)
@@ -100,42 +106,8 @@ class AccountsTableViewController: UIViewController, NSFetchedResultsControllerD
         self.present(alert, animated: true)
     }
 
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        let account = AccountController.shared.accounts[indexPath.row]
-//        AccountController.fetchMinersFromAccount(account: account) { (accountInfo) in
-//            print(accountInfo.totalHashrate)//FIXME: - Obvious
-//        }
-        
-    }
-
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        
-        if editingStyle == .delete {
-            let account = AccountController.shared.fetchResultsController.object(at: indexPath)
-            AccountController.shared.remove(account: account)
-        }
-    }
-    
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         if segue.identifier == "toMinersMain" {
@@ -147,28 +119,14 @@ class AccountsTableViewController: UIViewController, NSFetchedResultsControllerD
             }
         }
     }
-    
-    //MARK: - Colors / Design
-    
-    func areAccountsThere() {
-        if AccountController.shared.fetchResultsController.fetchedObjects?.count == 0 {
-            self.tableView.isHidden = true
-        } else {
-            self.tableView.isHidden = false
-        }
-    }
-
-    func addColors() { //FIXME: - I Don't really know what color to pick?
-        self.navigationController?.navigationBar.barTintColor = UIColor.backgroundDarkerGreen
-        self.tableView.backgroundColor = UIColor.backgroundTintGreen
-        self.noAccountsView.backgroundColor = UIColor.backgroundTintGreen
-    }
 
 }
 
 //MARK: - Helpers
 
 extension AccountsTableViewController {
+    
+    // Alerts
     
     func showAlertController(title: String, body: String) -> UIAlertController {
         // Create UIAlertController with the given title, and body text.
@@ -182,6 +140,22 @@ extension AccountsTableViewController {
         
         // Return the UIAlertController
         return alert
+    }
+    
+    // Colors / Design
+    
+    func areAccountsThere() {
+        if AccountController.shared.fetchResultsController.fetchedObjects?.count == 0 {
+            self.tableView.isHidden = true
+        } else {
+            self.tableView.isHidden = false
+        }
+    }
+
+    func addColors() { //TODO: - I Don't really know what color to pick?
+        self.navigationController?.navigationBar.barTintColor = UIColor.backgroundDarkerGreen
+        self.tableView.backgroundColor = UIColor.backgroundTintGreen
+        self.noAccountsView.backgroundColor = UIColor.backgroundTintGreen
     }
     
 }
